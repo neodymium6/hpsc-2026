e51dc0d1575553ab2957439e398a5058e0a7eb1d
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-04-27 15:58:27 +0900

    Vectorize nbody with AVX-512 intrinsics

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,26 +1,33 @@
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 16;
-  float x[N], y[N], m[N], fx[N], fy[N];
+  alignas(64) float x[N], y[N], m[N], fx[N], fy[N];
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+
+  __m512 xvec = _mm512_load_ps(x);
+  __m512 yvec = _mm512_load_ps(y);
+  __m512 mvec = _mm512_load_ps(m);
+
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m512 rx = _mm512_sub_ps(_mm512_set1_ps(x[i]), xvec);
+    __m512 ry = _mm512_sub_ps(_mm512_set1_ps(y[i]), yvec);
+    __m512 r2 = _mm512_add_ps(_mm512_mul_ps(rx, rx), _mm512_mul_ps(ry, ry));
+    __mmask16 mask = 0xffff ^ (1 << i);
+    __m512 inv_r = _mm512_rsqrt14_ps(r2);
+    __m512 inv_r3 = _mm512_mul_ps(_mm512_mul_ps(inv_r, inv_r), inv_r);
+    __m512 scale = _mm512_mul_ps(mvec, inv_r3);
+
+    fx[i] -= _mm512_mask_reduce_add_ps(mask, _mm512_mul_ps(rx, scale));
+    fy[i] -= _mm512_mask_reduce_add_ps(mask, _mm512_mul_ps(ry, scale));
+
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
