fe8840bf973287dcee75c5ca2221dab8139935c9
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2026-05-19 19:17:35 +0900

    Use cyclic hyperslabs for parallel HDF5

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <chrono>
 #include <vector>
+#include <mpi.h>
 #include "hdf5.h"
 using namespace std;
 
@@ -18,16 +19,17 @@ int main (int argc, char** argv) {
   hid_t dataset = H5Dopen(file, "dataset", H5P_DEFAULT);
   hid_t globalspace = H5Dget_space(dataset);
   int ndim = H5Sget_simple_extent_ndims(globalspace);
-  hsize_t N[ndim];
+  assert(ndim == 2);
+  hsize_t N[2];
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  hsize_t offset[2] = {mpirank / dim[1], mpirank % dim[1]};
+  hsize_t stride[2] = {dim[0], dim[1]};
+  hsize_t count[2] = {Nlocal[0], Nlocal[1]};
+  hsize_t block[2] = {1, 1};
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
   vector<int> buffer(Nlocal[0]*Nlocal[1]);
   plist = H5Pcreate(H5P_DATASET_XFER);
